59fcd2ea65077f0b192ddfa747e19d1770e4e452
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-12-21 00:57:34 +0530

    value shown in console after clicked in button

--- 3-calculator-version-one/calculator/src/App.jsx
+++ 3-calculator-version-one/calculator/src/App.jsx
@@ -1,13 +1,17 @@
 import styles from "./App.module.css";
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 function App() {
+      const [calVal, setCalVal] = useState("");
+      const onButtonClick = (buttonText) => console.log(buttonText);
+
   
   return (
     <>
       <div className={styles.calculator}>
-        <Display></Display>
-        <ButtonsContainer></ButtonsContainer>
+        <Display displayValue={calVal}></Display>
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
       </div>
       </>
   )
