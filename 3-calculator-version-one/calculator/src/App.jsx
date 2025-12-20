05c30cb755d1b0cae1a0b3b47bbe0ef222236ee5
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-12-21 01:08:48 +0530

    Calculation and all buttons are perform correctly

--- 3-calculator-version-one/calculator/src/App.jsx
+++ 3-calculator-version-one/calculator/src/App.jsx
@@ -4,8 +4,17 @@ import ButtonsContainer from "./components/ButtonsContainer";
 import { useState } from "react";
 function App() {
       const [calVal, setCalVal] = useState("");
-      const onButtonClick = (buttonText) => console.log(buttonText);
-
+      const onButtonClick = (buttonText) => {
+        if(buttonText === "C"){
+         setCalVal("");
+        }else if(buttonText === "="){
+        const result = eval(calVal);
+        setCalVal(result);
+        }else{
+          const newDisplayValue = calVal + buttonText;
+          setCalVal(newDisplayValue);
+        }
+      } 
   
   return (
     <>
