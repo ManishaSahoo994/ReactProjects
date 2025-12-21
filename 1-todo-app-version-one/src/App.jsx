763f53ad4fe9ff6a69f5f6608c96ad3ec0724c72
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-12-21 11:06:03 +0530

    value shown after clicking on add button and displayed

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -2,10 +2,11 @@ import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 import Todoitems from "./components/Todoitems";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
 
-  const todoItems = [{
+  const initialTodoItems = [{
     name: "Buy Milk",
     dueDate: "4/10/2025"
   },
@@ -18,10 +19,18 @@ function App() {
     dueDate: "right now"
   }];
 
+  const [todoItems, setTodoItems] = useState(initialTodoItems);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+  console.log(`New item added: ${itemName} Date:${itemDueDate}`);
+  const newTodoItems = [...todoItems, {name: itemName, dueDate: itemDueDate}];
+  setTodoItems(newTodoItems);
+  }
+
   return (<center
   class='todo-container'>
     <AppName></AppName>
-    <AddTodo></AddTodo>
+    <AddTodo onNewItem = {handleNewItem}></AddTodo>
     <Todoitems todoItems={todoItems}></Todoitems>
   </center>
   );
--- 1-todo-app-version-one/src/components/AddTodo.jsx
+++ 1-todo-app-version-one/src/components/AddTodo.jsx
@@ -1,14 +1,34 @@
-function AddTodo(){
-return  <div class="container text-center">
-  <div class="row kg-row">
-    <div class="col-6">
-     <input type="text" placeholder="Enter Todo here" />
+import { useState } from "react";
+
+function AddTodo({onNewItem}){
+
+  const [todoName, setTodoName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleNameChange = (event) => {
+  // console.log(event.target.value);
+   setTodoName(event.target.value);
+  }
+  const handleDateChange = (event) => {
+   setDueDate(event.target.value);
+  }
+
+  const handleAddButtonClicked = () => {
+    onNewItem(todoName, dueDate);
+    setTodoName("");
+    setDueDate(""); 
+  }
+
+return  <div className="container text-center">
+  <div className="row kg-row">
+    <div className="col-6">
+     <input type="text" placeholder="Enter Todo here" value={todoName} onChange={handleNameChange}/>
     </div>
-    <div class="col-4">
-      <input type="date" />
+    <div className="col-4">
+      <input type="date" value={dueDate} onChange={handleDateChange}/>
     </div>
-    <div class="col-2">
-      <button type="button" class="btn btn-success kg-button">Add</button>
+    <div className="col-2">
+      <button type="button" className="btn btn-success kg-button" onClick ={handleAddButtonClicked}>Add</button>
     </div>
   </div>
   </div>
