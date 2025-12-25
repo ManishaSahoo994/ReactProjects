383f7496b5d74004ec6aaa60d1bf5b9d9af53c97
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-12-25 21:31:53 +0530

    components are created in Social media app

---
+++ 4-social-media/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
+
+function App() {
+
+  return (
+
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="content">
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+      <Footer></Footer>
+      </div>
+    </div>
+  );
+};
+
+export default App;
