c7f7351e83e68d7a88895ea4cff9cb7569265c01
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-12-30 10:17:36 +0530

    Update Bharat Clock components

--- 2-bharat-clock-version-one/src/components/CurrentTime.jsx
+++ 2-bharat-clock-version-one/src/components/CurrentTime.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+
 //by using arrow function
 let CurrentTime = () => {
-    let time = new Date();
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        console.log("interval has been setup");
+        const intervalId = setInterval(() => {
+         setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+            console.log("Cancel the interval");
+        }
+
+    }, []);
+    
     return <p>This is the current time: {time.toLocaleDateString()} - {""}
     {time.toLocaleTimeString()}</p>;
 };
